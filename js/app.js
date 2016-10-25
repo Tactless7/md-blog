1d9f792223a7c613f0d052ed97ba7a319e81fa43
Juliane Blier <blier.juliane@gmail.com>
2016-10-25 10:52:10 +0200

    Récupération de l'article et du menu

--- js/app.js
+++ js/app.js
@@ -1,8 +1,49 @@
 (function(){
 	"use strict";
 	var app = {
+		article: null,
 		init:function(){
-			// let's go
+			this.getFile();
+		},
+		getFile: function(){
+			$.ajax({
+				url: 'http://192.168.1.21:1337/alice.md',
+				type: 'GET',
+				success: function(data){
+					console.log('success');
+					this.article = data;
+					app.displayAlice(app.transformMd(data));
+				},
+				error: function(){
+					console.log('error');
+				},
+				complete: function(){
+					console.log('complete');
+				}
+			});
+			$.ajax({
+				url: 'http://192.168.1.21:1337/menu.json',
+				type: 'GET',
+				success: function(data){
+					console.log('success');
+					console.log(data.menu[0].title);
+					console.log(data.menu[0].path);
+				},
+				error: function(){
+					console.log('error JSON');
+				},
+				complete: function(){
+					console.log('complete');
+				}
+			});
+		},
+		transformMd: function(text){
+			var converter = new showdown.Converter(),
+			html = converter.makeHtml(text);
+			return html;
+		},
+		displayAlice: function(html){
+			$('#md').html(html);
 		},
 	};
 
